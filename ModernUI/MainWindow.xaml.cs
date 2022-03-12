f483a09857e0c269fe4c22e4d390bc46baa954de
Murat <Murat@MURATLENOVOW10D>
2022-03-12 13:48:00 +0100

    Add project files.

---
+++ ModernUI/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace ModernUI
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+
+            {
+                DragMove();
+            }
+        }
+    }
+}
